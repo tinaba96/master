a8bf93479e53c31c75d047e3560aac9678ace546
tinaba96 <tinaba178.96@gmail.com>
2020-10-03 22:04:50 +0900

    first commit

---
+++ minor_research/conv_vitabi.m
@@ -0,0 +1,13 @@
+t = poly2trellis([4 3],[4 5 17;7 4 2]);
+
+x = ones(100,1);
+code = convenc(x,t);
+
+t = poly2trellis([4 3],[4 5 17;7 4 2]);
+
+code = convenc(ones(100,1),t);
+
+tb = 2;
+decoded = vitdec(code,t,tb,'trunc','hard');
+
+isequal(decoded,ones(100,1))
