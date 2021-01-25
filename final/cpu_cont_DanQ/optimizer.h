e7343dfee3dab1a601943ec18b1c92ac4f94cc8c
tinaba96 <tinaba178.96@gmail.com>
2021-01-25 13:32:43 +0900

    first commit

---
+++ final/cpu_cont_DanQ/optimizer.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+class rmsprop{
+public:
+	float lr = 0.01;
+	float decayrate = 0.99;
+	void update(float* params, float* grads, float* h, int size)
+	{
+		for(int i = 0; i < size; ++i)
+		{
+			h[i] *= decayrate;
+			h[i] += (1-decayrate)*grads[i]*grads[i];
+			params[i] -= lr*grads[i]/(0.000000001 + sqrt(h[i]));
+
+
+		}
+	}
+};
+
+
+
+
