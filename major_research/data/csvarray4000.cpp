d1a7558ac862b939b6c5c28302602236471eff13
tinaba96 <tinaba178.96@gmail.com>
2023-07-09 23:59:23 +0900

    training data

---
+++ major_research/data/csvarray4000.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+	  using namespace std;
+	    ifstream stream("sampledatax4000.csv");
+	    string line;
+	    double data[4000];
+		  const string delim = ",";
+
+		    int row = 0;
+	      int col;
+        while ( getline(stream, line) ) {
+  		    col = 0;
+ //delim‚ğ‹æØ‚è•¶š‚Æ‚µ‚ÄØ‚è•ª‚¯Aint‚É•ÏŠ·‚µ‚Ädata[][]‚ÉŠi”[‚·‚é
+     for ( string::size_type spos, epos = 0;
+      (spos = line.find_first_not_of(delim, epos)) != string::npos;) {
+       string token = line.substr(spos,(epos = line.find_first_of(delim, spos))-spos);
+      data[col++] = stoi(token);
+     }
+  }
+
+     for ( col = 0; col < 4000; ++col ) {
+       cout << data[col] << " ";
+     }
+       cout << endl;
+ }
+
+
+
