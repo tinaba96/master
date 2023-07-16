98cfaf86d7df308dfe36299ca505252481648138
tinaba96 <tinaba178.96@gmail.com>
2023-07-16 20:53:16 +0900

    update files

--- minor_research/MaxLUT LDPC/QuantDmc-4/startup.m
+++
@@ -1,7 +0,0 @@
-addpath([pwd '/quantDmc']);
-addpath([pwd '/curve']);
-
-if exist('quantBiDmc') ~= 3
-    warning(sprintf('QUANTBIDMC MEX file is not available for this platform\nRun MEXIFY to compile.\n'));
-    mexify
-end
